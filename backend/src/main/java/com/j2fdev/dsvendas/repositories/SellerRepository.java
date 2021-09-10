4dd6906de874dc5a00d6ac416a15b64f68dfd3b0
Jefferson Guedes Dias <jehguedes.work@gmail.com>
2021-09-09 22:41:34 -0300

    Layers

---
+++ backend/src/main/java/com/j2fdev/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.j2fdev.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.j2fdev.dsvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+}
